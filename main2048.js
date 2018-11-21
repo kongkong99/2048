77427add0b30dffbd644d750694c0078d65e4417
kongkong99 <932907376@qq.com>
2018-11-21 12:21:09 +0800

    web-PC-version1.1

--- main2048.js
+++ main2048.js
@@ -124,24 +124,28 @@ function generateOneNumber(){
 $(document).keydown( function( event ){
     switch( event.keyCode ){
         case 37: //left
+          event.preventDefault();
             if( moveLeft() ){
                 setTimeout("generateOneNumber()",210);
                 setTimeout("isgameover()",300);
             }
             break;
         case 38: //up
+          event.preventDefault();
             if( moveUp() ){
                 setTimeout("generateOneNumber()",210);
                 setTimeout("isgameover()",300);
             }
             break;
         case 39: //right
+          event.preventDefault();
             if( moveRight() ){
                 setTimeout("generateOneNumber()",210);
                 setTimeout("isgameover()",300);
             }
             break;
         case 40: //down
+          event.preventDefault();
             if( moveDown() ){
                 setTimeout("generateOneNumber()",210);
                 setTimeout("isgameover()",300);
